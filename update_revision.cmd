871824296831e2bab70226847988d8023e3be3be
marcmy <21000174+marcmy@users.noreply.github.com>
2026-09-26 04:25:21 -0400

    fix: generate valid revision header in detached CI checkout

--- update_revision.cmd
+++ update_revision.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-SETLOCAL
+SETLOCAL EnableDelayedExpansion
 CD /D %~dp0
 
 ECHO #pragma once > revision.h
@@ -17,21 +17,17 @@ IF NOT EXIST %gitexe% GOTO :END
 %gitexe% log -1 --date=format:%%Y.%%m.%%d --pretty=format:"#define REV_DATE %%ad%%n" >> revision.h
 %gitexe% log -1 --pretty=format:"#define REV_HASH %%h%%n" >> revision.h
 
-<nul set /p strTemp=#define REV_BRANCH >> revision.h
-%gitexe% symbolic-ref --short HEAD >> revision.h
-IF %ERRORLEVEL% NEQ 0 (
-ECHO LOCAL >> revision.h
-)
+SET "revbranch=LOCAL"
+FOR /F "delims=" %%I IN ('%gitexe% symbolic-ref --short -q HEAD 2^>NUL') DO SET "revbranch=%%I"
+ECHO #define REV_BRANCH !revbranch!>> revision.h
 
+SET "revnum=0"
 IF DEFINED MPCVR_TEST_REVISION (
-ECHO #define REV_NUM %MPCVR_TEST_REVISION% >> revision.h
+SET "revnum=%MPCVR_TEST_REVISION%"
 ) ELSE (
-<nul set /p strTemp=#define REV_NUM >> revision.h
-%gitexe% rev-list --count HEAD >> revision.h
-IF %ERRORLEVEL% NEQ 0 (
-ECHO 0 >> revision.h
+FOR /F %%I IN ('%gitexe% rev-list --count HEAD 2^>NUL') DO SET "revnum=%%I"
 )
-)
+ECHO #define REV_NUM !revnum!>> revision.h
 
 :END
 ENDLOCAL
